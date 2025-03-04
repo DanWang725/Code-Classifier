a9d9437347d8a9af92e4897a1772dec468a89244
DanWang725 <dw.smaug@gmail.com>
2025-03-04 13:08:11 -0500

    improve generation

--- datasets/VedranLjubovic/data_grabber.py
+++ datasets/VedranLjubovic/data_grabber.py
@@ -9,8 +9,8 @@ question_generation_prompt = """You will be given c langauge code from a file. Y
 
 Write the assignment question in clear, structured English, formatted into paragraphs, providing clear learning outcomes."""
 
-question_verification_prompt = "Verify that the following assignment question compares to one seen in a first year computer science course. If it is clear, include 'ANSWERYES' in your response. Otherwise include 'ANSWERNO' in your response."
-
+question_verification_prompt = "Verify that the following assignment question compares to one seen in a first year computer science course, and is written in only English, not including any variable or function names. Write either 'ANSWERYES' or 'ANSWERNO'. Verify the following text:"
+question_verification_prompt_2 = "Is this text in English words and have the an assignment title? Respond with 'ANSWERYES' or 'ANSWERNO'. Here is the text:"
 summarization_prompt = """Summarize the following assignment question into a single paragraph. The summary should be concise and capture what type of program needs to be written, and what problem it is solving. Do not include any new information in the summary. """
 
 #figure out how to format stuff
@@ -21,7 +21,7 @@ def retrieveResponse(prompt: str, question: str):
             'content': prompt + "\n\n" + question,
         }
     ])
-    sys.stderr.write(response.message.content)
+    # sys.stderr.write(response.message.content)
     cleaned_content = (str)(re.sub(r"<think>.*?</think>\n?", "", response.message.content, flags=re.DOTALL))
     return cleaned_content 
 
@@ -38,9 +38,8 @@ def open_pickle(file_path: str, columns: list):
       return pd.DataFrame(columns=columns)
 
 if __name__ == '__main__':
-  questionIdentifier = re.search(r'([A-Z]\d-[A-Z]\d)', sys.argv[1])[1]
+  questionIdentifier = re.search(r'([A-Z]\d-[A-Z]\d-\d+)', sys.argv[1])[1]
   questions = open_pickle("questions.pkl", ["question", "identifier"])
-  # codeOutput = open_pickle("programs.pkl")
 
   if(len(sys.argv) != 2):
     print("Usage: python data_grabber.py <input_file>")
@@ -48,23 +47,31 @@ if __name__ == '__main__':
   file = open(sys.argv[1], "r")
   code = "\n".join(file.readlines())  
   file.close()
+
   print("Generating Problem Statement")
   question_response = retrieveResponse(question_generation_prompt, code)
 
   print("Verifying Question")
+  retryCount = 0
   verification_response = retrieveResponse(question_verification_prompt, question_response)
-  print(verification_response)
-  if(verification_response.find("ANSWERYES") == -1):
-    print(question_response)
-    print("Question is not clear. Exiting....")
-    sys.exit(1)
-  else:
-    print("Question is clear")
+  while(verification_response.find("ANSWERYES") == -1):
+    print("Failed Verification")
+    retryCount += 1
+    if(retryCount >= 3):
+      sys.stderr.write(questionIdentifier + " RETRIED TOO MANY TIMES \n")
+      sys.exit(1)
+    
+    sys.stderr.write(questionIdentifier + " RETRY " + str(retryCount) + "\n")
+    print("Retry #" + str(retryCount) + ": Generating Problem Statement")
+    question_response = retrieveResponse(question_generation_prompt, code)
+    print("Verifying Question")
+    verification_response = retrieveResponse(question_verification_prompt, question_response)
+
+  print("Passed Verification")
+
   print("Generating Summary")
   summary_response = retrieveResponse(summarization_prompt, question_response)
   print("Saving....")
   questions = insert_df(questions, [question_response, questionIdentifier])
-  # codeOutput = codeOutput.append({"code": code, "identifier": questionIdentifier}, ignore_index=True)
   questions.to_pickle("questions.pkl")
-  # codeOutput.to_pickle("programs.pkl")
   print(summary_response)
