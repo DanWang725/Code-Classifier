d41da860b663bb34ca1a24d9d282f4d01e195c18
DanWang725 <dw.smaug@gmail.com>
2025-03-29 01:48:34 -0400

    upgrade embedding to queue up multiple files

--- src/generate_embeddings.py
+++ src/generate_embeddings.py
@@ -10,23 +10,17 @@ from utils.directories import prepared_dir
 from utils.file_retrieval import DataFileDirectory
 from utils.file_utils import prompt_save_file, EMBEDDING_EXTENSION
 from utils.embeddings_gen import get_embedding, get_embedding_roBERTa
-from utils.embedding import expand_embeddings_2
+from utils.embedding import expand_embeddings_2, ENCODERS, EncoderMap
 
 torch.cuda.empty_cache()
 
-checkpoint = "Salesforce/codet5p-110m-embedding"
 device = "cuda"  # for GPU usage or "cpu" for CPU usage
 
-encoders = ["neulab/codebert-c", "Salesforce/codet5p-110m-embedding"]
 def loadCode(filename: str):
   source = pd.read_pickle(filename)
   return source
 
-def generate(input_file: str, encoder: str):
-  tokenizer = AutoTokenizer.from_pretrained(encoder, trust_remote_code=True)
-  model = AutoModel.from_pretrained(encoder, trust_remote_code=True).to(device)
-  print("Loaded Model")
-
+def generate(tokenizer, model, input_file: str, encoderStr: str):
   source = loadCode(input_file)
   print(f"Loaded code. Count: {source.shape}")
   embeddings = source['code'].tolist()
@@ -35,35 +29,39 @@ def generate(input_file: str, encoder: str):
 
   with alive_bar(len(source)) as bar:
     for index, row in enumerate(embeddings):
-      if(encoder == encoders[0]):
+      if(encoderStr == ENCODERS[0]):
         embeddings[index] = get_embedding_roBERTa(row, tokenizer, model)
       else:
-        embeddings[index] = get_embedding(row, tokenizer, model, 1024)
+        embeddings[index] = get_embedding(row, tokenizer, model)
       bar()
 
   output['code_embeddings'] = embeddings
   return output
 
 if __name__ == "__main__":
-  for index, model in enumerate(encoders):
+  for index, model in enumerate(ENCODERS):
     print(f"{index}: {model}")
   choice = int(input("Choose Encoder"))
-  chosen_encoder = encoders[choice]
+  chosen_encoder = ENCODERS[choice]
+
+  tokenizer = AutoTokenizer.from_pretrained(chosen_encoder, trust_remote_code=True)
+  model = AutoModel.from_pretrained(chosen_encoder, trust_remote_code=True).to(device)
+  print("Loaded Model")
 
   file_path = os.path.dirname(os.path.abspath(__file__)) + "/" + prepared_dir
   question_files_class = DataFileDirectory(file_path, '.code.pkl')
   data_input_file = question_files_class.get_file("Select file to embed")
+  while data_input_file is not None:
+    data_input_file = question_files_class.get_file("Select file to embed, exit to continue.")
 
-  output_map, multiple = prompt_save_file(question_files_class, EMBEDDING_EXTENSION)
-  data_output_file = output_map[data_input_file]
-  
+  output_map, multiple = prompt_save_file(question_files_class, "-" + EncoderMap[chosen_encoder]+EMBEDDING_EXTENSION)
+  for data_input_file, data_output_file in output_map.items():
+    print("="*40)
+    print("Reading from: ", data_input_file)
+    print("Saving to: ", data_output_file)
+    print(f"Using {chosen_encoder}")
 
-  print("="*40)
-  print("Reading from: ", data_input_file)
-  print("Saving to: ", data_output_file)
-  print(f"Using {chosen_encoder}")
-
-  output = generate(data_input_file, chosen_encoder)
-  
-  output.to_pickle(data_output_file)
-  print("Saved to ", data_output_file)
+    output = generate(tokenizer, model, data_input_file, chosen_encoder)
+    
+    output.to_pickle(data_output_file)
+    print("Saved to ", data_output_file)
